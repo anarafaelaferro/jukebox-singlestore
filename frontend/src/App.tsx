9742b499ae2930c6db9003df5eeb413abc8bd9da
Rafaela <anarafaelaferro@gmail.com>
2024-10-24 14:43:40 +0100

    Convert to Typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,19 @@ import axios from 'axios';
 // Set the default base URL for all Axios requests
 axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 
+type Album = {
+  album_id: number;
+  album_name: string;
+  artist_name: string;
+};
+
 function App() {
-  const [albums, setAlbums] = React.useState([]);
+  const [albums, setAlbums] = React.useState<Array<Album>>([]);
 
   React.useEffect(() => {
     const fetchAlbums = async () => {
       try {
-        const response = await axios.get('/api/albums');
+        const response = await axios.get<Array<Album>>('/api/albums');
         setAlbums(response.data);
       } catch (error) {
         console.error('Error fetching albums:', error);
